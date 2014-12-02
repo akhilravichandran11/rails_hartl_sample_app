fab6bc76815537feac44de92fc18ed00502495cd
akhilravichandran11 <akhilravichandran11@gmail.com>
2014-12-02 20:06:57 +0000

    Delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update , :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_Action :admin_user, only: :destroy
   
   def show
     @user = User.find(params[:id])
@@ -6,6 +9,17 @@ class UsersController < ApplicationController
   
   def new
     @user = User.new
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
+  def index
+    ## @users = User.all
+    @users = User.paginate(page: params[:page])
   end
   
   def create
@@ -17,12 +31,43 @@ class UsersController < ApplicationController
     else 
       render 'new'
     end
-end
+  end
 
-private
+  def edit
+    @user = User.find(params[:id])
+  end
 
-def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
-end
+  def update
+  ##@user = User.find(params[:id])
+  if @user.update_attributes(user_params)
+    flash[:success] = "Profile updated"
+    redirect_to @user
+  else
+    render 'edit'
+  end
+  end
+ 
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+  def logged_in_user
+   unless logged_in?
+   store_location
+   flash[:danger] = "Please log in."
+   redirect_to login_url
+   end
+  end
+
+  def correct_user
+    @user = User.find(parmas[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+  
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141201173147) do
+ActiveRecord::Schema.define(version: 20141202161929) do
 
   create_table "relationships", force: true do |t|
     t.integer  "follower_id"
@@ -27,9 +27,10 @@ ActiveRecord::Schema.define(version: 20141201173147) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
